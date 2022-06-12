37ada76e6721ae231d3330b6316fa9902f9c5cba
Charlie Kaylor <68200523+ckaylor33@users.noreply.github.com>
2022-06-12 21:29:15 +0100

    Added connection string in startup.cs & connectionService in data folder

--- Startup.cs
+++ Startup.cs
@@ -37,7 +37,7 @@ namespace TheBlogFinalMVC
 
             services.AddDbContext<ApplicationDbContext>(options =>
             options.UseNpgsql(
-            Configuration.GetConnectionString("DefaultConnection")));
+            ConnectionService.GetConnectionString(Configuration)));
 
             services.AddDatabaseDeveloperPageExceptionFilter();
 
