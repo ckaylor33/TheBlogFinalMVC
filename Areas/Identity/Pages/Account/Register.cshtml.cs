1a0a3df7307c2cecd0e665a38716f6b0cf91e735
Charlie Kaylor <68200523+ckaylor33@users.noreply.github.com>
2022-09-01 09:22:37 +0100

    Changes to register

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -139,15 +139,9 @@ namespace TheBlogFinalMVC.Areas.Identity.Pages.Account
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
-                        return RedirectToPage("./RegisterConfirmation");
-                    }
-                    else
-                    {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
-                    }
+
+                    return RedirectToPage("./RegisterConfirmation");
+
                 }
                 foreach (var error in result.Errors)
                 {
